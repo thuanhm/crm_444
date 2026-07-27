7b10eba85eea74d8a460d052c4dc92d730a88a6a
thuanhm <75974825+thuanhm@users.noreply.github.com>
2026-07-27 16:27:37 +0700

    Update Google Fonts link in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Glory:wght@400;500;600;700;800&family=JetBrains+Mono:wght@400;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&family=JetBrains+Mono:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
